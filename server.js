37745ca381782fd4444ea1faa75a15a061bdac60
Paul Prokosch <paul.prokosch@gmail.com>
2018-02-25 13:50:58 -0600

    Setup initial files complete.  Finished GET Methods for home and survey and capability to display JSON for friends array

---
+++ server.js
@@ -0,0 +1,57 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require ("path");
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+//require("./routing/apiRoutes")(app);
+//require("./app/routing/html-routes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+//array of objects for our friends
+var possibleFriends = [
+	{
+	 name: "Heidi",
+	 photo: "http://via.placeholder.com/350x150",
+	 scores: [1,1,1,1,1,1,1,1,1,1]
+	}
+];
+app.get ("/",function(req,res){
+	res.sendFile(path.join(__dirname, "/../FriendFinder/app/public/index.html"));
+});
+app.get('/survey', function (req, res) {
+  res.sendFile(path.join(__dirname,"/../FriendFinder/app/public/survey.html"))
+});
+app.get('/api/friends', function (req,res) {
+  res.json(possibleFriends);
+});
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
